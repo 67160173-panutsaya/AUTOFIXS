4ef50be987f3cb5ea8ebc5155e71839703c2b998
67160173-panutsaya <67160173@go.buu.ac.th>
2026-08-21 14:56:11 +0700

    Fix GitHub Pages config

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,7 +1,10 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+const supabaseUrl =
+  import.meta.env.VITE_SUPABASE_URL || 'https://thbwybzlrzducxzmuhqg.supabase.co';
+const supabaseAnonKey =
+  import.meta.env.VITE_SUPABASE_ANON_KEY ||
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRoYnd5YnpscnpkdWN4em11aHFnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODcyOTE3MDIsImV4cCI6MjEwMjg2NzcwMn0.azjO3qUKaKXzL8oYUufSuNMlm980Y_-hI2xQOX3aeYM';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,8 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { fileURLToPath, URL } from 'node:url';
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: process.env.NODE_ENV === 'production' ? '/AUTOFIXS/' : '/',
   plugins: [react()],
   resolve: {
     alias: {
